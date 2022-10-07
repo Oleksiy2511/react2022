570f8e69f19b6ea8d69b7cc1dbafd60e5fc9d70d
Oleksiy2511 <kurulenko275@gmail.com>
2022-10-07 23:50:17 +0300

    simpson

---
+++ src/components/simpsons/Simpson.js
@@ -0,0 +1,13 @@
+
+function Simpson(props) {
+    const {simpson} = props;
+    return (
+        <div>
+            <h3>{simpson.name}  {simpson.surname}--{simpson.age}</h3>
+            <p>{simpson.info}</p>
+            <img src={simpson.photo} alt={simpson.name}/>
+        </div>
+    );
+}
+
+export default Simpson;
