5b110b027d9d1f7547accf7c941e876e7dc5c3ff
Oleksiy2511 <kurulenko275@gmail.com>
2022-10-08 16:37:35 +0300

    simpsons new

--- src/components/simpsons/Simpson.js
+++ src/components/simpsons/Simpson.js
@@ -3,7 +3,9 @@ function Simpson(props) {
     const {simpson} = props;
     return (
         <div className="simpson">
-            <h3>{simpson.name}  {simpson.surname} = {simpson.age}</h3>
+            <div className="nameAge">
+                <h3>{simpson.name}  {simpson.surname} = {simpson.age}</h3>
+            </div>
             <img src={simpson.photo} alt={simpson.name}/>
             <p>{simpson.info}</p>
 
